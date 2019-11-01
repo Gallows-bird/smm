4b6ebce07dff99bc8449a77a2793634f8215c47e
Gallows-bird <kutetsky93@gmail.com>
2019-11-01 16:12:20 +0500

    some js

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,15 @@
-$('.nav-toggle').on('click', function() {
-    $(this).toggleClass('nav-toggle--active');
+$(document).ready(function() {
+
+    $('.nav-toggle').on('click', function() {
+        $(this).toggleClass('nav-toggle--active');
+    });
+
+
+    $('.index-top').fadeIn(800);
+
+    $('.index-btn-top').animate(
+        {
+            // marginTop: '20px',
+            opacity: 1
+        }, 1000);
 });

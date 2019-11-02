148a755bdc13e20c3d1f585b0c6e015eb1773708
Gallows-bird <kutetsky93@gmail.com>
2019-11-02 20:25:24 +0500

    price

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,19 @@
-$('.nav-toggle').on('click', function() {
-    $(this).toggleClass('nav-toggle--active');
+$(document).ready(function() {
+
+    $(function() {
+        var burgerAnimate = $('.nav-toggle').on('click', function() {
+            $(this).toggleClass('nav-toggle--active');
+        });
+    });
+
+    $('.index-top').fadeIn(800);
+
+    $(function() {
+        var indexBtnTop = $('.index-btn-top');
+        indexBtnTop.animate(
+            {
+                marginTop: '50px',
+                opacity: 1
+            }, 800);
+    });
 });

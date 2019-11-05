5b97b12e9d94edf809ec497ef4c82aae6e42a213
Gallows-bird <kutetsky93@gmail.com>
2019-11-05 16:38:09 +0500

    price after js

--- app/js/main.js
+++ app/js/main.js
@@ -10,10 +10,42 @@ $(document).ready(function() {
 
     $(function() {
         var indexBtnTop = $('.index-btn-top');
-        indexBtnTop.animate(
-            {
-                marginTop: '50px',
-                opacity: 1
-            }, 800);
+        indexBtnTop.animate({
+            marginTop: '50px',
+            opacity: 1
+        }, 800);
+    });
+
+    var tab = document.querySelectorAll('.order-tab'),
+        headerTab = document.querySelector('.order-panel'),
+        tabContent = document.querySelectorAll('.price-tab');
+
+    function hideTabContent(a) {
+        for (var i = a; i < tabContent.length; i++) {
+            tabContent[i].classList.remove('active');
+            tabContent[i].classList.add('hide');
+        }
+    }
+    hideTabContent(1);
+
+    function showTabContent(b) {
+        if (tabContent[b].classList.contains('hide')) {
+            tabContent[b].classList.remove('hide');
+            tabContent[b].classList.add('active');
+        }
+    }
+
+    headerTab.addEventListener('click', function(event) {
+        var target = event.target;
+        if (target && target.classList.contains('order-tab')) {
+            for (var i = 0; i < tabContent.length; i++) {
+                if (target == tab[i]) {
+                    hideTabContent(0);
+                    showTabContent(i);
+                    break;
+                }
+            }
+        }
+
     });
 });
